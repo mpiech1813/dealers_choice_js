1ed7955b491f5526b1df369dec492ebdac9f0cc1
Maciej Pie <maciejpiech18@gmail.com>
2021-02-15 17:26:55 -0600

    added the table

--- seed.sql
+++ seed.sql
@@ -1,5 +1,18 @@
 TRUNCATE TABLE model;
 
+CREATE TABLE brand (
+  id SERIAL PRIMARY KEY,
+  name TEXT DEFAULT NULL
+);
+
+CREATE TABLE model (
+  id SERIAL PRIMARY KEY,
+  model TEXT, 
+colour TEXT,
+material TEXT,
+brand_id INTEGER,
+);
+
 INSERT INTO model (model, colour, material, brand_id) VALUES ('Impact', 'Black', 'PU', (SELECT id FROM brand WHERE name='Venum'));
 INSERT INTO model (model, colour, material, brand_id) VALUES ('Elite', 'Green', 'Leather', (SELECT id FROM brand WHERE name='Venum'));
 INSERT INTO model (model, colour, material, brand_id) VALUES ('Trooper', 'Trooper', 'PU', (SELECT id FROM brand WHERE name='Venum'));
